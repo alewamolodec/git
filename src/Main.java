8028eca60bd26e8d84235f9a1b17ef1be8ee1b0f
Alex Strashnov <airAlex00@mail.ru>
2023-02-27 00:03:29 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int i =1; i<10;i++ ){
+            System.out.print(i + " ");
+            for (int j = 9; j>0;j--){
+                System.out.print(j*i);
+                System.out.println("");
+            }
+        }
+    }
+}
